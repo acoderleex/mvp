a9153ff15ec677ad7ddceb2515026920bc59873a
xf.li@8win.com <xf.li@8win.com>
2016-07-11 18:40:43 +0800

    optimize code

---
+++ app/src/main/java/com/example/tony/mvp/adapters/ZYOtherAdapter.java
@@ -0,0 +1,34 @@
+package com.example.tony.mvp.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tony on 7/11/16.
+ */
+public class ZYOtherAdapter extends RecyclerView.Adapter<ZYOtherAdapter.ZYOtherAdapterHolder> {
+
+    @Override
+    public ZYOtherAdapterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ZYOtherAdapterHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ZYOtherAdapterHolder extends RecyclerView.ViewHolder {
+
+        public ZYOtherAdapterHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+}
